12ada7b2158518dbb405bfcd3c6a5368220bab64
Matthew Hand <matthew.hand@datacom.com.au>
2023-11-05 18:38:28 +1100

    moved create-file into fileroutes added more file locking

--- src/routes/commandRoutes.ts
+++ src/routes/commandRoutes.ts
@@ -2,19 +2,11 @@ import express from 'express';
 import ServerHandlerSingleton from '../services/serverHandlerInstance';
 import { ServerConfig } from '../types';
 import config from 'config';
-import lockfile from 'proper-lockfile';
 
 // Define interfaces for expected request body to improve type safety
 interface RunCommandRequestBody {
   command: string;
   timeout?: number;
-}
-
-interface CreateFileRequestBody {
-  filename: string;
-  content: string;
-  backup?: boolean;
-  directory?: string;
 }
 
 const router = express.Router();
@@ -32,45 +24,13 @@ router.post('/run', async (req, res) => {
   const { command, timeout }: RunCommandRequestBody = req.body;
   const effectiveTimeout = timeout || config.get<number>('commandTimeout') || 180000;
 
-  // Define a unique lock name based on the command, or use a generic one if the command should not run concurrently with any other
-  const lockName = `command-lock-${command}`;
-
   try {
-    // Attempt to acquire a lock before executing the command
-    const release = await lockfile.lock(lockName, { realpath: false });
-
-    try {
-      const result = await serverHandler.executeCommand(command, effectiveTimeout);
-      res.status(200).json(result);
-    } finally {
-      // Release the lock after the command execution
-      await release();
-    }
+    const result = await serverHandler.executeCommand(command, effectiveTimeout);
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error instanceof Error ? error.message : 'Unknown error' });
   }
 });
 
-
-router.post(['/create-file', '/create-or-replace-file'], async (req, res) => {
-  const { filename, content, backup = true, directory = "" }: CreateFileRequestBody = req.body;
-  
-  if (!serverHandler) {
-    return res.status(500).json({ error: 'Server handler not initialized' });
-  }
-
-  try {
-    // Await the getCurrentDirectory if it's asynchronous
-    const targetDirectory = directory || await serverHandler.getCurrentDirectory();
-    const success = await serverHandler.createFile(targetDirectory, filename, content, backup);
-    if (success) {
-      res.json({ message: 'File created successfully' });
-    } else {
-      res.status(400).json({ error: 'Failed to create file' });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err instanceof Error ? err.message : 'Unknown error' });
-  }
-});
 
 export default router;
--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -41,17 +41,36 @@ router.post('/set-current-folder', async (req, res) => {
   }
   
 });
-// Create or replace a file
+
+// Combined create or replace a file route
 router.post(['/create-file', '/create-or-replace-file'], async (req, res) => {
-  const { filename, content, backup = true, directory = "" } = req.body;
+  const { filename, content, backup = true, directory } = req.body;
   const serverHandler = getServerHandler(res);
   if (!serverHandler) return;
 
-  const targetDirectory = directory || serverHandler.getCurrentDirectory();
+  const targetDirectory = directory || await serverHandler.getCurrentDirectory();
+  const fullPath = path.join(targetDirectory, filename);
 
-  serverHandler.createFile(targetDirectory, filename, content, backup)
-    .then(() => res.status(200).json({ message: 'File created or replaced successfully.' }))
-    .catch((err: Error) => res.status(500).json({ error: err.message }));
+  try {
+    // Lock the file to prevent concurrent writes
+    const release = await lockfile.lock(fullPath, { realpath: false });
+
+    try {
+      // Perform the file creation or replacement
+      const success = await serverHandler.createFile(targetDirectory, filename, content, backup);
+      if (success) {
+        res.status(200).json({ message: 'File created or replaced successfully.' });
+      } else {
+        res.status(400).json({ error: 'Failed to create or replace file.' });
+      }
+    } finally {
+      // Always release the lock
+      await release();
+    }
+  } catch (err) {
+    // Handle errors
+    res.status(500).json({ error: err instanceof Error ? err.message : 'Unknown error' });
+  }
 });
 
 router.post('/update-file', async (req, res) => {
@@ -97,11 +116,29 @@ router.post('/amend-file', async (req, res) => {
   const serverHandler = getServerHandler(res);
   if (!serverHandler) return;
 
-  const targetDirectory = directory || serverHandler.getCurrentDirectory();
+  const targetDirectory = directory || await serverHandler.getCurrentDirectory();
+  const fullPath = path.join(targetDirectory, filename);
 
-  serverHandler.amendFile(path.join(targetDirectory, filename), content)
-    .then(() => res.status(200).json({ message: 'File amended successfully.' }))
-    .catch((err: Error) => res.status(500).json({ error: err.message }));
+  try {
+    // Lock the file to prevent concurrent writes
+    const release = await lockfile.lock(fullPath, { realpath: false });
+
+    try {
+      // Perform the file amendment
+      await serverHandler.amendFile(fullPath, content);
+      res.status(200).json({ message: 'File amended successfully.' });
+    } catch (err) {
+      // If an error occurs, respond with the error message
+      res.status(500).json({ error: err instanceof Error ? err.message : 'Unknown error' });
+    } finally {
+      // Always release the lock
+      await release();
+    }
+  } catch (err) {
+    // If locking fails, respond with the error message
+    res.status(500).json({ error: err instanceof Error ? err.message : 'Locking failed' });
+  }
 });
+
 
 export default router;
