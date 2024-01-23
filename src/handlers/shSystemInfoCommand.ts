1941b2b5181f420a0fa7b236b5e41c6b5bbdc07f
Matthew Hand <matthewhandau@gmail.com>
2024-01-23 21:19:14 +1100

    Refactor system info commands into separate modules for better code organization
    - Moved psSystemInfoCmd and shSystemInfoCmd into separate TypeScript files.
    - Updated LocalServerHandler to import these commands for modular code structure.
    - This change enhances maintainability and readability of the system information retrieval logic.

---
+++ src/handlers/shSystemInfoCommand.ts
@@ -0,0 +1,13 @@
+export const shSystemInfoCmd = `
+echo "{
+  \"homeFolder\": \"$HOME\",
+  \"type\": \"$(uname -o | tr -d '\n')\",
+  \"release\": \"$(uname -r | tr -d '\n')\",
+  \"platform\": \"$(uname -m | tr -d '\n')\",
+  \"architecture\": \"$(lscpu | grep Architecture | awk '{print $2}' | tr -d '\n')\",
+  \"totalMemory\": \"$(free -m | grep Mem: | awk '{print $2}' | tr -d '\n')\",
+  \"freeMemory\": \"$(free -m | grep Mem: | awk '{print $7}' | tr -d '\n')\",
+  \"uptime\": \"$(awk '{print $1}' /proc/uptime | tr -d '\n')\",
+  \"currentFolder\": \"$(pwd | tr -d '\n')\"
+}"
+`;
