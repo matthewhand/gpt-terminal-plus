c03f6be6b50916a7c9d92dddc01db3611b0cbc0a
Matthew Hand <matthewhandau@gmail.com>
2024-07-27 03:29:39 +0000

    chore(types): add custom module declarations for TypeScript

---
+++ custom.d.ts
@@ -0,0 +1,7 @@
+declare module 'ssh2-sftp-client';
+declare module 'util';
+declare module 'fs/promises';
+declare module 'uuid';
+declare module 'debug';
+declare module 'config';
+declare module 'proper-lockfile';
