fdc44476b277bc730b64e97a879c5590a91abaff
Matthew Hand <matthewhandau@gmail.com>
2024-07-30 08:25:57 +0000

    Remove unused custom.d.ts file
    The custom.d.ts file was found to be unused and was removed to clean up the project structure. Verified through successful tests, linting, and build processes.

--- custom.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'ssh2-sftp-client';
-declare module 'util';
-declare module 'fs/promises';
-declare module 'uuid';
-declare module 'debug';
-declare module 'config';
-declare module 'proper-lockfile';
