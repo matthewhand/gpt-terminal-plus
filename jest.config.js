d999ff510b1eb90da1c3bf0f1b33c0f98da35743
Matthew Hand <matthewhandau@gmail.com>
2024-08-05 11:00:01 +0000

    Add jest.config.js for configuring Jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   roots: ['<rootDir>/tests', '<rootDir>/src/tests'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/',
   },
   setupFiles: ['module-alias/register'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
 };
