c39d3fb7bb97eb1b6358cd8af6e7de9d57bb6e06
Matthew Hand <matthewhandau@gmail.com>
2024-08-13 03:05:56 +0000

    refactor(routes): update setServer to use hostname
    - Simplified the logic to directly pass `hostname` for ServerManager.

--- src/routes/server/setServer.ts
+++ src/routes/server/setServer.ts
@@ -6,40 +6,30 @@ import { handleServerError } from '../../utils/handleServerError';
 
 const debug = Debug('app:server:setSelectedServer');
 
-/**
- * Function to set the selected server.
- * @param {Request} req - The Express request object.
- * @param {Response} res - The response object.
- */
 export const setServer = async (req: Request, res: Response) => {
   const { server } = req.body;
   debug('Received request to set server: ' + server, { requestBody: req.body });
 
   try {
-    // Validate the server before setting it
     const serverConfig = ServerManager.getServerConfig(server);
     if (!serverConfig) {
       throw new Error('Server not in predefined list.');
     }
 
-    // Validate that the protocol is defined
     if (!serverConfig.protocol) {
       throw new Error('Unsupported protocol: undefined');
     }
 
-    // Set the selected server using the global state helper
-    setServerHelper(server);
-    debug('Server set to ' + server + ' using global state helper.');
+    setServerHelper(serverConfig.hostname);
+    debug('Server set to ' + serverConfig.hostname + ' using global state helper.');
 
-    // Create an instance of ServerManager
-    const serverManager = new ServerManager(serverConfig);
+    const serverManager = new ServerManager(serverConfig.hostname);
 
-    // Fetch the server handler instance using the updated server
     const ServerHandler = serverManager.createHandler();
-    ServerHandler.setServerConfig(serverConfig); // Set the server config
-    debug('ServerHandler instance successfully retrieved for server: ' + server);
+    ServerHandler.setServerConfig(serverConfig);
+    debug('ServerHandler instance successfully retrieved for server: ' + serverConfig.hostname);
 
-    res.status(200).json({ message: 'Server set to ' + server });
+    res.status(200).json({ message: 'Server set to ' + serverConfig.hostname });
   } catch (error) {
     debug('Error in /server/set', {
       error,
