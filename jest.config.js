90645697dd7b8120b0a1db30e49a747848ba24e3
Matthew Hand <matthewhandau@gmail.com>
2024-09-29 22:19:38 +0000

    Update Jest configuration: Enhance module mapping, transform options, and environment setup.

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,28 @@
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["html", "text", "lcov"],
-
-  roots: ['<rootDir>/tests'],
-  transform: {
-    '^.+\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/',
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@common/(.*)$": "<rootDir>/src/common/$1",
+    "^@command/(.*)$": "<rootDir>/src/command/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@llm/(.*)$": "<rootDir>/src/llm/$1",
+    "^@message/(.*)$": "<rootDir>/src/message/$1",
+    "^@webhook/(.*)$": "<rootDir>/src/webhook/$1",
+    "^@integrations/(.*)$": "<rootDir>/src/integrations/$1",
+    "^@types/(.*)$": "<rootDir>/src/types/$1"
   },
-  setupFiles: ['module-alias/register'],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(chai)/)"
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json"
+    }
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
 };
