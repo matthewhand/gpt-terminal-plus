19a68e006e96e0fde8ba5ede8a397778e038e3f6
Matthew Hand <matthewhandau@gmail.com>
2024-10-02 22:08:51 +0000

    chore(cleanup): Remove old tests/ directory and align test file locations
    - Deleted the outdated tests/ directory now that tests are in src/tests/.
    - Minor adjustments to LocalServerHandler.ts and LocalServerConfig.ts.

--- tests/utils/localServerUtil.ts
+++
@@ -1,5 +0,0 @@
-import LocalServer from "../../src/handlers/local/LocalServerHandler";
-
-export const createLocalServer = () => {
-  return new LocalServer({ hostname: "localhost", protocol: "local", code: true });
-};
--- tests/utils/test-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const testConfig = {
-    host: "localhost",
-    protocol: "local",
-};
-exports.default = testConfig;
